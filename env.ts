a59dedb175130fb6718ef208beb13d1b9022e61c
sawin <samuelalegre022@gmail.com>
2024-02-12 10:51:45 -0300

    ----

--- env.ts
+++ env.ts
@@ -1,3 +1,3 @@
 // Ejemplo "https://2a4a-34-106-9-117.ngrok-free.app/upload"
 
-export const apiURL = "https://5651-35-197-92-89.ngrok-free.app/upload"
+export const apiURL = "https://5e13-34-123-35-214.ngrok-free.app/upload"
