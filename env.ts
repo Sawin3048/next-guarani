a939f48acac874d0dddffe107cf9e0378396de6d
Samuel3048 <samuelalegre022@gmail.com>
2024-02-13 21:18:35 -0300

    actualizar link

--- env.ts
+++ env.ts
@@ -1,3 +1,3 @@
 // Ejemplo "https://2a4a-34-106-9-117.ngrok-free.app/upload"
 
-export const apiURL = "https://5e13-34-123-35-214.ngrok-free.app/upload"
+export const apiURL = "https://2519-34-136-138-102.ngrok-free.app/upload"
