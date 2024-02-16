03bc46f66ee410016f98c607c254cff0f05e684f
sawin <samuelalegre022@gmail.com>
2024-02-16 15:21:17 -0300

    ---

--- env.ts
+++ env.ts
@@ -1,6 +1,6 @@
 // Ejemplo "https://2a4a-34-106-9-117.ngrok-free.app/upload"
 
-export const transcriptorApiUrl = "https://6366-34-80-38-202.ngrok-free.app/upload"
+export const transcriptorApiUrl = "https://f7b2-34-145-188-115.ngrok-free.app/upload"
 let url = ''
 
 if (process.env.NODE_ENV === 'development') {
