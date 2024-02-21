9155e7d47d18f8d031ee2fd2beb22605720c623a
sawin <samuelalegre022@gmail.com>
2024-02-21 09:40:56 -0300

    --

--- src/app/api/level/[id]/level4.ts
+++ src/app/api/level/[id]/level4.ts
@@ -17,7 +17,7 @@ export const level4: Chapter = {
     id: 'uqq-f9iP4VEzxvPW-jnoq',
     type: 'complete-with-audio',
     data: {
-      imageSrc: '/public/level-souces/sDN4Txrc1Sxx3kDMjG1ik.png',
+      imageSrc: '/public/level-souces/j5qsKL4XkB7y3PcJdZ0tt.png',
       words: [{ type: 'word', word: "Péa ha'e" }, { type: 'space' }],
       options: ['Mbarakaja morotĩ', 'Vaka piru', 'Jagua karape', 'Vaka Kyra'],
       correctOption: ['Mbarakaja morotĩ']
@@ -27,7 +27,7 @@ export const level4: Chapter = {
     id: 'DHU2ouiX-P2XI9n-2U91R',
     type: 'complete-with-audio',
     data: {
-      imageSrc: '/public/level-souces/sDN4Txrc1Sxx3kDMjG1ik.png',
+      imageSrc: '/public/level-souces/MOSPtJsIeVYFPLPQLRZnZ.png',
       words: [{ type: 'word', word: "Péa ha'e" }, { type: 'space' }],
       options: ['Vaka Kyra', "Óga ky'a", 'Mbarakaja hu'],
       correctOption: ['Vaka Kyra']
@@ -37,7 +37,7 @@ export const level4: Chapter = {
     id: 'YxuSfSwGElHEqHfowrRFI',
     type: 'complete-with-audio',
     data: {
-      imageSrc: '/public/level-souces/sDN4Txrc1Sxx3kDMjG1ik.png',
+      imageSrc: '/public/level-souces/P306bft7ha5kZF0bryAxX.png',
       words: [{ type: 'word', word: "Péa ha'e" }, { type: 'space' }],
       options: ['Panambi vera', "Óga ky'a", 'Korapy tuicha'],
       correctOption: ["Óga ky'a"]
@@ -47,7 +47,7 @@ export const level4: Chapter = {
     id: 'J_Gs7-eAeQnW1rnWpJd1a',
     type: 'complete-with-audio',
     data: {
-      imageSrc: '/public/level-souces/sDN4Txrc1Sxx3kDMjG1ik.png',
+      imageSrc: '/public/level-souces/wFuuny5Ot1IxbPfXhbgAD.png',
       words: [{ type: 'word', word: "Péa ha'e" }, { type: 'space' }],
       options: ['Korapy hovy', "Ao potĩ", 'Tapiti potĩ'],
       correctOption: ["Ao potĩ"]
@@ -57,7 +57,7 @@ export const level4: Chapter = {
     id: 'RzvvgI0R8HJsBet1xU7lu',
     type: 'complete-with-audio',
     data: {
-      imageSrc: '/public/level-souces/sDN4Txrc1Sxx3kDMjG1ik.png',
+      imageSrc: '/public/level-souces/byplVXNruAX0GqQZaUUZL.png',
       words: [{ type: 'word', word: "Péa ha'e" }, { type: 'space' }],
       options: ['korapy tuichá', "yvyra pytã", 'yvy hu'],
       correctOption: ["korapy tuichá"]
@@ -77,7 +77,7 @@ export const level4: Chapter = {
     id: 'jgVXt2NBBlO6XcjRyx5wH',
     type: 'complete-with-audio',
     data: {
-      imageSrc: '/public/level-souces/sDN4Txrc1Sxx3kDMjG1ik.png',
+      imageSrc: '/public/level-souces/b1k55YL4rQy5yBbqEwNz4.png',
       words: [{ type: 'word', word: "Péa ha'e" }, { type: 'space' }],
       options: ["Akã apu'a", "Kuarahy mimbi", 'Jyva mbarete'],
       correctOption: ["Yvyra tuichá"]
--- src/app/level/page.tsx
+++ src/app/level/page.tsx
@@ -8,7 +8,7 @@ export default function Level() {
           <LevelSelector btnUrl="level/NymsQz0MvUCY2XqDQEnRJ" color="pink" dificulty="fácil" imgSrc="/niño-buscando.png" title="Etapa 1: Terarãngue" subTitle="Pronombres Personales"/>
         </li>    
         <li>
-          <LevelSelector btnUrl="level/Osr4xJtV1X9H41Db6vYOR" color="purple" dificulty="fácil" imgSrc="/niña-comiendo.png" title="Etapa 2: Ñe’ẽtéva" subTitle="Verbos"/>
+          <LevelSelector btnUrl="level/Osr4xJtV1X9H41Db6vYOR" color="purple" dificulty="fácil" imgSrc="/niña-comiendo.png" title="Etapa 2: Ñe'ẽtéva" subTitle="Verbos"/>
         </li>
         <li>
           <LevelSelector btnUrl="level/fx1GsK8Jmk4rC6dgw53BM" color="emerald" dificulty="fácil" imgSrc="/niño-buscando.png" title="Etapa 3: Tero" subTitle="Sustantivos"/>
